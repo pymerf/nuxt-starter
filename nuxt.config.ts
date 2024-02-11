d58914507f6a347a0932dbfcefef899eae1bd7ad
Blessed Sibanda <pymerf@gmail.com>
2024-02-11 17:50:57 +0200

    Setup vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify']
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({autoImport: true}))
+      })
+    }
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls
+      }
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Nuxt Starter</h1>
+</template>
