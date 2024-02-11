f0d4f9e3223c3166fea020a8650c1a269d317510
Blessed Sibanda <pymerf@gmail.com>
2024-02-11 18:27:21 +0200

    Create home page and nav-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <v-app>
-    <slot />
+    <v-app-bar color="indigo-darken-4">
+      <template v-slot:prepend>
+        <v-btn to="/" icon="mdi-home"></v-btn>
+      </template>
+      <v-app-bar-title>Nuxt Starter</v-app-bar-title>
+
+      <v-btn to="/users">Users</v-btn>
+      <v-btn to="/signup">SignUp</v-btn>
+      <v-btn to="/signin">SignIn</v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container class="pt-8">
+        <slot />
+      </v-container>
+    </v-main>
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>Nuxt Starter</h1>
+  <VRow justify="center">
+    <VCol sm="10" md="8">
+      <VCard width="100%">
+        <VCardTitle>Home Page</VCardTitle>
+        <VImg src="~/assets/images/unicornbike.jpg" alt="Unicorn Bicycle" />
+        <VCardText>Welcome to Nuxt Starter</VCardText>
+      </VCard>
+    </VCol>
+  </VRow>
 </template>
